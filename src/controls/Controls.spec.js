abb18c4f604a2cdfb4cd6977b335e6249a091320
Mikey Williams <manicdesigner@gmail.com>
2019-10-30 20:36:58 -0400

    lock gate to unlock gate

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,8 +1,19 @@
 // Test away!
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, queryByText, findByText } from '@testing-library/react';
 import Controls from './Controls';
+import { fireEvent, act } from 'react-testing-library';
 
 test('Controls renders correctly', () => {
 	expect(render(<Controls />)).toMatchSnapshot();
+});
+
+test('Lock gate turns to unlock gate when fired', async () => {
+	const { getByText } = render(<Controls />);
+	const gate = getByText(/lock gate/i);
+
+	act(() => {
+		fireEvent.click(gate);
+	});
+	findByText(/unlock gate/i);
 });
