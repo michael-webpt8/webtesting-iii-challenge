582cee8be8df6689cb0aead93703e525183720b5
Mikey Williams <manicdesigner@gmail.com>
2019-10-30 20:03:26 -0400

    controls snapshot

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import Controls from './Controls';
+
+test('Controls renders correctly', () => {
+	expect(render(<Controls />)).toMatchSnapshot();
+});
