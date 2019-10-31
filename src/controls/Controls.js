6ec59ce2aa4bcbbe26e13096c557fce6a15ff5ad
Michael Williams <manicdesigner@gmail.com>
2019-10-31 12:17:29 -0400

    add data-tesid locking-feature && gate-feature

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
-const Controls = (props) => {
-	const { locked, closed, toggleLocked, toggleClosed } = props;
+const Controls = props => {
+  const { locked, closed, toggleLocked, toggleClosed } = props;
 
-	return (
-		<div className="controls panel">
-			<button
-				disabled={!closed}
-				onClick={toggleLocked}
-				className="toggle-btn"
-				data-testid="gate-1"
-			>
-				{locked ? 'Unlock Gate' : 'Lock Gate'}
-			</button>
-			<button disabled={locked} onClick={toggleClosed} className="toggle-btn">
-				{closed ? 'Open Gate' : 'Close Gate'}
-			</button>
-		</div>
-	);
+  return (
+    <div className='controls panel'>
+      <button
+        disabled={!closed}
+        onClick={toggleLocked}
+        className='toggle-btn'
+        data-testid='locking-feature'
+      >
+        {locked ? 'Unlock Gate' : 'Lock Gate'}
+      </button>
+      <button
+        disabled={locked}
+        onClick={toggleClosed}
+        className='toggle-btn'
+        data-testid='gate-feature'
+      >
+        {closed ? 'Open Gate' : 'Close Gate'}
+      </button>
+    </div>
+  );
 };
 
 export default Controls;
