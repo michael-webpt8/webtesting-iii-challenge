2baa41a5444bacbf9284ee1e2ce82991f73e6b1e
Michael Williams <manicdesigner@gmail.com>
2019-10-31 13:58:05 -0400

    add dashboard snapshot

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('Renders display correctly', () => {
+  expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+// test('set Gate to Closed on close Gate Button Click', () => {
+//   const setToggleLockedMock = jest.fn();
+//   const {  } = render(
+//     <Dashboard toggleLocked={setToggleLockedMock} />
+//   );
+//   const lockingButton = getByTestId('locking-feature');
+
+//   fireEvent.click(lockingButton);
+//   expect(setDisplayClosedToOpenMock).toHaveBeenCalled();
+// });
