ee6003127b80df129b1a654b7958a7e2bc279ca1
Michael Williams <manicdesigner@gmail.com>
2019-10-31 13:00:36 -0400

    test => close gate unlock gate close the gate and unlock the gate last call will be unlocked. added comments also

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Controls from './Controls';
-import { fireEvent, act } from 'react-testing-library';
 
 test('Controls renders correctly', () => {
   expect(render(<Controls />)).toMatchSnapshot();
 });
 
+/**
+ * close gate open gate
+ * state of gate is = locked then unlocked.
+ */
 test('close can turn to open = unlocked to locked', () => {
   const mockGateFeature = jest.fn();
   mockGateFeature(/close gate/i);
@@ -17,13 +20,30 @@ test('close can turn to open = unlocked to locked', () => {
   expect(mockGateFeature).toHaveBeenLastCalledWith(/open gate/i);
 });
 
-// kinda worthless test i think... :/
-test('Lock gate turns to unlock gate when fired', async () => {
+/**
+ * close the gate and unlock the gate
+ * will be both locked and locked later.
+ */
+
+test('close gate, and unlock gate', () => {
+  const mockGateFeature = jest.fn();
   const mockLockFeature = jest.fn();
-  mockLockFeature(/lock gate/i);
+
+  mockGateFeature(/close gate/i);
   mockLockFeature(/unlock gate/i);
-  mockLockFeature(/lock gate/i);
+
+  expect(mockGateFeature).toHaveBeenCalled();
   expect(mockLockFeature).toHaveBeenCalled();
-  expect(mockLockFeature).toHaveBeenCalledTimes(3);
-  expect(mockLockFeature).toHaveBeenLastCalledWith(/lock gate/i);
+  expect(mockLockFeature).toHaveBeenLastCalledWith(/unlock gate/i);
 });
+
+// // kinda worthless test i think... :/
+// test('Lock gate turns to unlock gate when fired', async () => {
+//   const mockLockFeature = jest.fn();
+//   mockLockFeature(/lock gate/i);
+//   mockLockFeature(/unlock gate/i);
+//   mockLockFeature(/lock gate/i);
+//   expect(mockLockFeature).toHaveBeenCalled();
+//   expect(mockLockFeature).toHaveBeenCalledTimes(3);
+//   expect(mockLockFeature).toHaveBeenLastCalledWith(/lock gate/i);
+// });
