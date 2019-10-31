7f94135847b49926a0000983f56e4a5706a41cc4
Mikey Williams <manicdesigner@gmail.com>
2019-10-30 20:03:40 -0400

    test todo

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,3 +6,11 @@ import Display from './Display';
 test('render Display correctly', () => {
 	expect(render(<Display />)).toMatchSnapshot();
 });
+
+//TODO test('closed class is true : red-led && locked');
+
+//TODO test('closed class is false: green-led && unlocked');
+
+//TODO test('closed true: closed && unlocked');
+
+//TODO test('closed true: closed and locked');
